37e6e2a5d37de37a088bbdba307e68cbfe0dce67
xuhaibo <xuhaibo@ebigdata.org>
2018-09-03 14:59:44 +0800

    上传代码

---
+++ src/main/java/com/haibo/test/web/controller/SelectionController.java
@@ -0,0 +1,51 @@
+package com.haibo.test.web.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.haibo.test.service.HttpRequestService;
+import com.haibo.test.utils.HttpClientUtil;
+import com.xdbigdata.framework.web.model.JsonResponse;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author XD
+ */
+@RestController
+@RequestMapping("/api")
+@Api(description = "移动端选课接口列表")
+public class SelectionController {
+    @Autowired
+    private HttpRequestService httpRequestService;
+    private HttpClientUtil httpClientUtil = null;
+    private HttpClientUtil httpClient = null;
+    private static String charset = "utf-8";
+    private static String customer = "iH1wXuzdLqr6An16MOgpJhaHMHWbambYYyjAcMjLFI68e6tKBtUUJ9Un_NLYohUmzlMLwp_XulALwFxJYiBadxB2s_9mw5zabDtaYr3CRtPuHlQYk9G3AVSum9HtpK9eOejmTymhkqD7Rf2LtQ3ZzMrKZr4NCpTrmm6jRqMSg9M%3D";
+    private static String rankUrl = "https://www.jiyou-tech.com/2018/287/php/get_rank.php";
+    private static String infoUrl = "https://www.jiyou-tech.com/2018/287/php/get_rank.php";
+
+    @GetMapping("/getRank")
+    public JsonResponse getRank() {
+        httpClientUtil = new HttpClientUtil();
+        Map<String, String> createMap = new HashMap<String, String>();
+        createMap.put("customer", customer);
+        String httpOrgCreateTestRtn = httpClientUtil.doPost(rankUrl, createMap, charset);
+        JSONObject jsonObject = JSONObject.parseObject(httpOrgCreateTestRtn);
+        return JsonResponse.success(jsonObject);
+    }
+
+    @GetMapping("/getInfo")
+    public JsonResponse getInfo() {
+        httpClientUtil = new HttpClientUtil();
+        Map<String, String> createMap = new HashMap<String, String>();
+        createMap.put("customer", customer);
+        String httpOrgCreateTestRtn = httpClientUtil.doPost(infoUrl, createMap, charset);
+        JSONObject jsonObject = JSONObject.parseObject(httpOrgCreateTestRtn);
+        return JsonResponse.success(jsonObject.toJSONString());
+    }
+}
