328866c88eb251b85c58ce00251c482d57a0964c
xuhaibo <xuhaibo@ebigdata.org>
2018-11-21 17:48:04 +0800

    上传代码

--- src/main/java/com/haibo/test/mapper/CustomerMapper.java
+++
@@ -1,7 +0,0 @@
-package com.haibo.test.mapper;
-
-import com.haibo.test.model.domain.Customer;
-import com.xdbigdata.framework.mybatis.mapper.TKMapper;
-
-public interface CustomerMapper extends TKMapper<Customer> {
-}
