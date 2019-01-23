49a7f39f3665aaab57cc14f0714c26c30baed76a
xuhaibo <xuhaibo@ebigdata.org>
2019-01-23 16:06:24 +0800

    上传代码

--- src/main/java/com/haibo/test/service/impl/NonghangServiceImpl.java
+++ src/main/java/com/haibo/test/service/impl/NonghangServiceImpl.java
@@ -17,7 +17,7 @@ public class NonghangServiceImpl {
     private static String cookie = "route=86c65ab2abd1087881cc394e170d71ac; orderNo=YH181208231540596083; BIGipServerpool_sclf_yx_web=2364402442.47138.0000; BIGipServerpool_sclf_yh=3186420490.47138.0000; BIGipServerpool_sclf_yh_web=3186420490.47138.0000; sclfyxlife_abchina_mobile_id=1D1HRM262TDK93EDEB0A0000506770EC";
     private static String userAgent = "Mozilla/5.0 (Linux; Android 8.0.0; MI 5s Build/OPR1.170623.032; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 BankabcAndroid/3.9.7 SDKVersion/26";
 
-    @Scheduled(cron = "58 59 9 * * ? ")
+    @Scheduled(cron = "58 59 11 * * ? ")
     public void testMain() throws Exception {
         for (int i = 0; i < 100000000; i++) {
             JSONObject createMap = new JSONObject();
