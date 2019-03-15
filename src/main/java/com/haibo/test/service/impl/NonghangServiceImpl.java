b3fb047e2fc5ac695996299d9cd8727e1cc53c3d
xuhaibo <xuhaibo@ebigdata.org>
2019-03-15 09:09:29 +0800

    上传代码

--- src/main/java/com/haibo/test/service/impl/NonghangServiceImpl.java
+++ src/main/java/com/haibo/test/service/impl/NonghangServiceImpl.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -31,25 +32,25 @@ public class NonghangServiceImpl {
     private static String getDateIdUrl = "https://wx.healthych.com/order/subscribe/departmentWorkTimes.do?";
     private static String commiturl = "https://wx.healthych.com/order/subscribe/add.do?";
     private static String getId = "https://wx.healthych.com/base/department/vaccines.do?";
-    private static String vaccCode = "8801";
-    private static String testDate = "20190301,20190304,20190305,20190307";
-    List<String> strings = Arrays.asList("5106030008");
+    private static String vaccCode = "8803";
+    List<String> strings = Arrays.asList("5101151308");
 
     @Scheduled(fixedRate = 100)
     @Test
     public void testMain() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("mm:ss SSS");
         Integer departmentVaccineId = null;
         for (String adress : strings) {
             String url1 = getId + "depaCode=" + adress + "&vaccineCode=" + vaccCode;
             String result1 = HttpClientUtil.doGet(url1, null);
             JSONObject jsStr1 = JSONObject.parseObject(result1);
-            System.out.println("getId" + jsStr1.toJSONString());
+            System.out.println(sdf.format(new Date()) + "getId" + jsStr1.toJSONString());
             JsonRootBean jsonRootBean = JSONObject.toJavaObject(jsStr1, JsonRootBean.class);
             departmentVaccineId = jsonRootBean.getData().get(0).getId();
             String url2 = getDateUrl + "depaCode=" + adress + "&vaccCode=" + vaccCode + "&departmentVaccineId=" + departmentVaccineId + "&vaccineIndex=1";
             String result2 = HttpClientUtil.doGet(url2, null);
             JSONObject jsStr2 = JSONObject.parseObject(result2);
-            System.out.println("getDateUrl" + jsStr2.toJSONString());
+            System.out.println(sdf.format(new Date()) +"getDateUrl" + jsStr2.toJSONString());
             ReturnDate returnDate = JSONObject.toJavaObject(jsStr2, ReturnDate.class);
             if (returnDate != null) {
                 if (returnDate.getCode().equals("0000") && returnDate.isOk() && CollectionUtils.isNotEmpty(returnDate.getData().getDateList())) {
@@ -58,6 +59,7 @@ public class NonghangServiceImpl {
                         String url3 = getDateOrder + "depaCode=" + adress + "&vaccCode=" + vaccCode + "&vaccIndex=1&days=" + DateUtil.convertListDate(returnDate.getData().getDateList()) + "&departmentVaccineId=" + departmentVaccineId;
                         String result3 = HttpClientUtil.doGet(url3, null);
                         JSONObject jsStr3 = JSONObject.parseObject(result3);
+                        System.out.println(sdf.format(new Date()) +"getDateOrder" + jsStr3.toJSONString());
                         DateOrder dateOrder = JSONObject.toJavaObject(jsStr3, DateOrder.class);
                         List<Data3> data3s = dateOrder.getData();
                         for (Data3 date3 : data3s) {
@@ -67,7 +69,7 @@ public class NonghangServiceImpl {
                                 String url4 = getDateIdUrl + "depaCode=" + adress + "&vaccCode=" + vaccCode + "&vaccIndex=1&subsribeDate=" + dateValue + "&departmentVaccineId=" + departmentVaccineId + "&linkmanId=545831";
                                 String result4 = HttpClientUtil.doGet(url4, null);
                                 JSONObject jsStr4 = JSONObject.parseObject(result4);
-                                System.out.println("getDateIdUrl" + jsStr4.toJSONString());
+                                System.out.println(sdf.format(new Date()) +"getDateIdUrl" + jsStr4.toJSONString());
                                 ReturnDateId returnDateId = JSONObject.toJavaObject(jsStr4, ReturnDateId.class);
                                 if (returnDateId != null) {
                                     if (returnDateId.isOk() && returnDateId.getCode().equals("0000")) {
