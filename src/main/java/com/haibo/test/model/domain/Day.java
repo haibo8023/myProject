f5cd896ec39f92f3fc8f4f47fefe119b97631e11
xuhaibo <xuhaibo@ebigdata.org>
2019-06-03 14:45:43 +0800

    上传代码

--- src/main/java/com/haibo/test/model/domain/Day.java
+++ src/main/java/com/haibo/test/model/domain/Day.java
@@ -1,11 +1,24 @@
 
 package com.haibo.test.model.domain;
 
-import lombok.Data;
-
-@Data
 public class Day {
 
     private String day;
     private Long total;
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
 }
